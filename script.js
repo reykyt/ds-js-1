7c630c3ea1d2fd9c168ae60d478148c186b059db
reykyt <145888308+reykyt@users.noreply.github.com>
2024-09-28 09:54:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+let x = 5;
+let y = 'hello';
+let z = true;
+let w = null;
+let v; 
+console.log(typeof (x));
+console.log(typeof (y));
+console.log(typeof (z));
+console.log(typeof (w));
+console.log(typeof (v));
+
+let k = parseInt ("10.5")
+console.log(k);
+
+let h = parseInt ("20")
+console.log(h);
+
+let j = parseInt ("30.25px")
+console.log(j);
+
+
+const user = "Джон";
+const age = 30;
+console.log(`Меня зовут ${user}, мне ${age} лет`)
+
+alert ("Я изучаю JavaScript")
+
+
+let num =123;
+
+alert (`${num}`);
+
+let a = 1+2+3;
+alert (`${a}`);
+
+
+
+let one =  parseInt(prompt("1число"));
+let two = parseInt(prompt("2 число"));
+
+let amount = (one) + (two);
+console.log (amount);
+
+let difference = (one) - (two);
+console.log (difference);
+
+let composition = (one) * (two);
+console.log (composition);
+
+
+let private = (one) / (two);
+console.log (private);
